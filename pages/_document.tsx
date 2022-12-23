5e74d89543bdda6145fbc4334ebe037493b4cb8f
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2022-12-23 19:16:03 +0100

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import ButtonAppBar from "../components/navbar";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <CssBaseline />
       <Head />
       <body>
+        <ButtonAppBar />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ pages/api/openai.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+//   console.log(req.body);
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `write this email in business form: ${req.body}`,
+    temperature: 0.9,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6,
+    // stop: [" Email:", " AI:"],
+  });
+  //   console.log(completion, "##");
+
+  //   res.status(200).json({ name: "John Doe" });
+  //@ts-ignore
+  return res.status(200).json(response.data.choices[0].text);
+}
